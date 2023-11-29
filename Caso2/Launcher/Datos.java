2591a8e02a636f536264e0570b98a88626515871
Makx3 <gatoconb42@gmail.com>
2023-11-29 10:13:15 -0300

    creacion de clases y gui form

---
+++ Caso2/Launcher/Datos.java
@@ -0,0 +1,9 @@
+package Caso2.Launcher;
+import Caso2.Vista.menu;
+
+public class Datos {
+    public static void main(String[] args) {
+        menu ventana = new menu();
+        ventana.setVisible(true);
+    }
+}
