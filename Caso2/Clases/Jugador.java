9c9dc38cfdaeaadaed5eeac681dab3f4f56bab4b
Makx3 <gatoconb42@gmail.com>
2023-11-29 10:32:37 -0300

    clase jugador

--- Caso2/Clases/Jugador.java
+++ Caso2/Clases/Jugador.java
@@ -3,5 +3,18 @@ package Caso2.Clases;
 public class Jugador {
 
     private Jugador nuevoJugador;
+    String nombreBasket, rutJugador;
 
+    public Jugador(  String nombreBasket, String rutJugador){
+        this.nombreBasket = nombreBasket;
+        this.rutJugador = rutJugador;
+    }
+
+    public String getNombreBasket() {
+        return nombreBasket;
+    }
+
+    public void setNombreBasket(String nombreBasket) {
+        this.nombreBasket = nombreBasket;
+    }
 }
